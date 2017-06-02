4b653953ca4e3b69a4bd62a0e9f3875d3f0ee17e
liyabo01 <liyabo01@baidu.com>
2017-06-02 19:34:49 +0800

    add eval method

---
+++ eval_method.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+#_*_coding: utf-8 _*_
+
+
+import numpy as np
+
+
+def eval(predict, la):
+    f_lst = []
+    weight = 1.0 / np.log([2, 3, 4, 5, 6])
+    for ps, qs in zip(predict, la):
+        #pred = [1.0 if q == p else 0.0 for q, p in zip(ps, qs)]
+        #pred = [1.0 if q in ps else 0.0 for q in qs]
+        pred = [1.0 if p in qs else 0.0 for p in ps]
+
+        acc = sum(weight * pred)
+        rel = sum(pred) / len(la)
+        f = acc * rel / (acc + rel)
+        f_lst.append(f)
+    return sum(f_lst)/len(f_lst)
+
+
+if __name__ == "__main__":
+    predict = np.random.randint(0, 3, size = (10, 5))
+    la = np.random.randint(0, 3, size = (10, 4))
+    print(predict)
+    print(la)
+    print(eval(predict, la))
